db1ad49713dbd4bf7b4bb33fe02d06a56ac82798
maeda-takumi <jiangqiantian54@gmail.com>
2026-03-17 11:08:51 +0900

    11

--- login_check.php
+++ login_check.php
@@ -7,6 +7,10 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
 }
 
 if (empty($_SESSION['logged_in'])) {
-    header('Location: /login/index.php?next=' . rawurlencode('/?page=index'));
+    $scriptDir = str_replace('\\', '/', dirname((string)($_SERVER['SCRIPT_NAME'] ?? '/')));
+    $basePath = $scriptDir === '/' ? '' : rtrim($scriptDir, '/');
+    $loginPath = $basePath . '/login/index.php';
+
+    header('Location: ' . $loginPath . '?next=' . rawurlencode('/?page=index'));
     exit;
 }
